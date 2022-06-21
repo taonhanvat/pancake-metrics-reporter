f73787c3405912cf4f22d1e1d688aff6512f973e
NGUYEN DUC THUAN <thuan.nd101299@gmail.com>
2022-06-21 12:30:33 +0700

    up

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule MetricsCake.MixProject do
   defp deps do
     [
       {:telemetry_metrics, "~> 0.5.0"},
-      {:plug, "~> 1.6"},
+      {:plug, "~> 1.7.0"},
       {:plug_cowboy, "~> 1.0"},
       {:plug_crypto, "~> 1.1.2"},
       {:jason, "~> 1.0"}
